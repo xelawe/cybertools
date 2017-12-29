6fa685128151b2e91473ec21aba5fe89ad68f603
xelawe <alexander@wernhart.net>
2017-12-29 01:10:12 +0100

    delta

--- cy_wunderg.cpp
+++ cy_wunderg.cpp
@@ -16,15 +16,18 @@ void cy_wunderg::send_val( char *iv_field, float iv_value ){
   DebugPrintln(_gv_host);
 
   // Use WiFiClient class to create TCP connections
-  WiFiClient client;
-  const int httpPort = 80;
-  if (!client.connect(_gv_host, httpPort)) {
-    DebugPrintln("connection failed");
-    return;
-  }
+  //WiFiClient client;
+
+//  const int httpPort = 80;
+//  if (!client.connect(_gv_host, httpPort)) {
+//    DebugPrintln("connection failed");
+//    return;
+//  }
 
   // We now create a URI for the request
-  url = "/weatherstation/updateweatherstation.php?action=updateraw&ID=";
+  url = "http://";
+  url += _gv_host;
+  url += "/weatherstation/updateweatherstation.php?action=updateraw&ID=";
   url += _gv_sid;
   url += "&PASSWORD=";
   url += _gv_pwd;
@@ -37,20 +40,42 @@ void cy_wunderg::send_val( char *iv_field, float iv_value ){
   DebugPrintln(url);
 
   // This will send the request to the server
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + _gv_host + "\r\n" +
-               //"Authorization: Basic " + wunderg_auth + "\r\n" +
-               "Connection: close\r\n\r\n");
-  delay(10);
-
+//  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+//               "Host: " + _gv_host + "\r\n" +
+//               //"Authorization: Basic " + wunderg_auth + "\r\n" +
+//               "Connection: close\r\n\r\n");
+//  delay(10);
+	
   // Read all the lines of the reply from server and print them to Serial
-  while (client.available()) {
-    String line = client.readStringUntil('\r');
-    DebugPrint(line);
-  }
+//  while (client.available()) {
+//    String line = client.readStringUntil('\r');
+//    DebugPrint(line);
+//  }
 
-  DebugPrintln();
-  DebugPrintln("closing connection");
+//  DebugPrintln();
+//  DebugPrintln("closing connection");
+	
+	HTTPClient http;
+
+	    //http.begin("https://192.168.1.12/test.html", "7a 9c f4 db 40 d3 62 5a 6e 21 bc 5c cc 66 c8 3e a1 45 59 38"); //HTTPS
+	http.begin(url); //HTTP
+    int httpCode = http.GET();
+	   // httpCode will be negative on error
+    if (httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+     Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+      // file found at server
+      if (httpCode == HTTP_CODE_OK) {
+        String payload = http.getString();
+        Serial.println(payload);
+      }
+    } else {
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+
+    http.end();
+
 	
 }
 
